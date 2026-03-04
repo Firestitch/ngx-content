5c499acaf3799372159a118ae4098cc677b052ca
Raymond Gigliotti <ray@firestitch.com>
2026-03-04 05:59:05 -0500

    Update package version to 18.0.4 and remove unused Angular Material imports in fs-content-pages.module.ts

--- src/app/modules/content-pages/fs-content-pages.module.ts
+++ src/app/modules/content-pages/fs-content-pages.module.ts
@@ -1,29 +1,4 @@
-import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatTooltipModule } from '@angular/material/tooltip';
-
-import { FsCommonModule } from '@firestitch/common';
-import { FsDateModule } from '@firestitch/date';
-import { FsDialogModule } from '@firestitch/dialog';
-import { FsFormModule } from '@firestitch/form';
-import { FsHtmlEditorModule } from '@firestitch/html-editor';
-import { FsLabelModule } from '@firestitch/label';
-import { FsListModule } from '@firestitch/list';
-import { FsSkeletonModule } from '@firestitch/skeleton';
-import { FsTextEditorModule } from '@firestitch/text-editor';
-
-
 
 import { ContentPageEditorComponent } from './components';
 import { ContentPageComponent } from './components/content-page';
@@ -31,31 +6,10 @@ import { FsContentPagesComponent } from './components/content-pages';
 
 @NgModule({
     imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule,
-    MatDialogModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatTabsModule,
-    MatIconModule,
-    MatSelectModule,
-    MatButtonToggleModule,
-    MatTooltipModule,
-    FsListModule,
-    FsDateModule,
-    FsCommonModule,
-    FsFormModule,
-    FsLabelModule,
-    FsSkeletonModule,
-    FsHtmlEditorModule,
-    FsDialogModule,
-    FsTextEditorModule,
-    FsContentPagesComponent,
-    ContentPageComponent,
-    ContentPageEditorComponent,
-],
+        FsContentPagesComponent,
+        ContentPageComponent,
+        ContentPageEditorComponent,
+    ],
     exports: [
         FsContentPagesComponent,
     ],
